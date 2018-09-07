9518a74373ba7f26e006493143ef6c7ae83a4e87
huy <leduchuy2801.com>
2018-09-07 16:52:52 +0700

    dssinhvien

---
+++ src/Huy/dev/Dssinhvien.java
@@ -0,0 +1,21 @@
+package Huy.dev;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class Dssinhvien {
+	
+		public static void main (String[]args) {
+			try {
+				FileInputStream fis = new FileInputStream("in.txt");
+				BufferedReader br = new BufferedReader (new InputStreamReader(fis));
+				String line ;
+			while ((line = br.readLine()) !=null) {
+					System.out.println(line);
+				}
+				br.close ();
+			} catch (Exception e) {
+				
+			}		
+		}
+}
